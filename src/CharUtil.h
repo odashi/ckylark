36a7f221793f3e303b7ae0a918de24139d215341
odashi <yus.takara@gmail.com>
2014-10-22 20:54:08 +0900

    add CharUtil and some utilities of StringUtil

---
+++ src/CharUtil.h
@@ -0,0 +1,26 @@
+#ifndef CKYLARK_CHAR_UTIL_H_
+#define CKYLARK_CHAR_UTIL_H_
+
+namespace Ckylark {
+
+// utility functions for character processing
+class CharUtil {
+
+    CharUtil() = delete;
+    CharUtil(const CharUtil &) = delete;
+    CharUtil & operator=(const CharUtil &) = delete;
+
+public:
+
+    // make uppercase character
+    inline static char toUpper(char c) { return (c >= 'a' && c <= 'z') ? c + 'A' - 'a' : c; }
+    
+    // make lowercase character
+    inline static char toLower(char c) { return (c >= 'A' && c <= 'Z') ? c + 'a' - 'Z' : c; }
+
+}; // class CharUtil
+
+} // namespace Ckylark
+
+#endif // CKYLARK_CHAR_UTIL_H_
+
--- src/StringUtil.h
+++ src/StringUtil.h
@@ -1,6 +1,9 @@
 #ifndef CKYLARK_STRING_UTIL_H_
 #define CKYLARK_STRING_UTIL_H_
 
+#include "CharUtil.h"
+
+#include <algorithm>
 #include <string>
 
 namespace Ckylark {
@@ -38,6 +41,47 @@ public:
 
     // true if str contains lowercase alphabet, false otherwise
     inline static bool hasLower(const std::string & str) { return hasCharRange(str, 'a', 'z'); }
+
+    // count the number of character c in str
+    inline static int numChar(const std::string & str, char c) {
+        int n = 0;
+        for (char x : str) {
+            if (x == c) ++n;
+        }
+        return n;
+    }
+
+    // count the number of characters in str which is in the range [low, high]
+    inline static int numCharRange(const std::string & str, char low, char high) {
+        int n = 0;
+        for (char x : str) {
+            if (x >= low && x <= high) ++n;
+        }
+        return n;
+    }
+
+    // count the number of digits in str
+    inline static int numDigit(const std::string & str) { return numCharRange(str, '0', '9'); }
+
+    // count the number of uppercase alphabet in str
+    inline static int numUpper(const std::string & str) { return numCharRange(str, 'A', 'Z'); }
+
+    // count the number of lowercase alphabet in str
+    inline static int numLower(const std::string & str) { return numCharRange(str, 'a', 'z'); }
+
+    // make uppercase string
+    inline static std::string toUpper(const std::string & str) {
+        std::string ret = str;
+        std::transform(ret.begin(), ret.end(), ret.begin(), CharUtil::toUpper);
+        return ret;
+    }
+
+    // make lowercase string
+    inline static std::string toLower(const std::string & str) {
+        std::string ret = str;
+        std::transform(ret.begin(), ret.end(), ret.begin(), CharUtil::toLower);
+        return ret;
+    }
 
 }; // class StringUtil
 
