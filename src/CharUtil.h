0beb410aebccfbff945c7f719ec45379f1d784f8
odashi <yus.takara@gmail.com>
2014-10-23 01:58:46 +0900

    add some functions into CharUtil

--- src/CharUtil.h
+++ src/CharUtil.h
@@ -12,11 +12,26 @@ class CharUtil {
 
 public:
 
+    // true if c is one of whitespace or control code of ASCII, false otherwise
+    inline static bool isControl(char c) { return ((c >= 0x00 && c <= 0x20) || c == 0x7f); }
+
+    // true if c is real symbol character, false otherwise
+    inline static bool isLetter(char c) { return !isControl(c); }
+
+    // true if c is one of whitespace, false otherwise
+    inline static bool isSpace(char c) { return ((c >= 0x09 && c <= 0x0d) || c == 0x20); }
+
+    // true if c is one of uppercase alphabet, false otherwise
+    inline static bool isUpper(char c) { return (c >= 'A' && c <= 'Z'); }
+    
+    // true if c is one of lowercase alphabet, false otherwise
+    inline static bool isLower(char c) { return (c >= 'a' && c <= 'z'); }
+
     // make uppercase character
-    inline static char toUpper(char c) { return (c >= 'a' && c <= 'z') ? c + 'A' - 'a' : c; }
+    inline static char toUpper(char c) { return isLower(c) ? c + 'A' - 'a' : c; }
     
     // make lowercase character
-    inline static char toLower(char c) { return (c >= 'A' && c <= 'Z') ? c + 'a' - 'A' : c; }
+    inline static char toLower(char c) { return isUpper(c) ? c + 'a' - 'A' : c; }
 
 }; // class CharUtil
 
